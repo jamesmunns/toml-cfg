05fb810e1d14d88d6409b6bdd1204dc79bb3a5d8
Dániel Buga <bugadani@gmail.com>
2023-10-31 21:16:20 +0100

    Add option to set a value converter, assume enumeration if default is a path

--- pkg-example/lib-one/src/lib.rs
+++ pkg-example/lib-one/src/lib.rs
@@ -1,7 +1,37 @@
 #![no_std]
 
+#[derive(Debug)]
+pub enum Choice {
+    One,
+    Other,
+    Third,
+}
+
+impl Choice {
+    const fn from_int(n: usize) -> Choice {
+        match n {
+            0 => Choice::One,
+            1 => Choice::Other,
+            _ => Choice::Third,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum OtherChoice {
+    Foo,
+    Bar,
+}
+
 #[toml_cfg::toml_config]
 pub struct Config {
     #[default(32)]
     buffer_size: usize,
+
+    #[default(Choice::A)]
+    #[convert(Choice::from_int)]
+    choice: Choice,
+
+    #[default(OtherChoice::Foo)]
+    other_choice: OtherChoice,
 }
